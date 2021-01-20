a74a00943eb14d3a7d9c842ec1538081e5e56618
magatha <77102773+magatha@users.noreply.github.com>
2021-01-20 20:22:28 +0100

    Update 1.Importing_data_from_flat_files_with_utils.r

--- 1.R_Courses/Introduction_to_importing_Data_in_R/1.Importing_data_from_flat_files_with_utils.r
+++ 1.R_Courses/Introduction_to_importing_Data_in_R/1.Importing_data_from_flat_files_with_utils.r
@@ -10,7 +10,10 @@ str(pools)
 
 -----
   
-
+# stringsAsFactors
+With stringsAsFactors, you can tell R whether it should convert strings in the flat file to factors.
+For all importing functions in the utils package, this argument is TRUE, which means that you import strings as factors. This only makes sense if the strings you import represent categorical variables in R. If you set stringsAsFactors to FALSE, the data frame columns corresponding to strings in your text file will be character.
+Youll again be working with the swimming_pools.csv file. It contains two columns (Name and Address), which shouldnt be factors.
 
 
 
