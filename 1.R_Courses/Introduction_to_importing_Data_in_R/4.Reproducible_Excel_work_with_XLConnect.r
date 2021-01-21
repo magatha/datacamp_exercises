031e6281b53d84c1934e95cfcf479ccd465df2c1
magatha <77102773+magatha@users.noreply.github.com>
2021-01-21 09:56:28 +0100

    Update and rename 4.Reproducible_Excel_work_with_XLConnect to 4.Reproducible_Excel_work_with_XLConnect.r

---
+++ 1.R_Courses/Introduction_to_importing_Data_in_R/4.Reproducible_Excel_work_with_XLConnect.r
@@ -0,0 +1,4 @@
+# Connect to a workbook
+When working with XLConnect, the first step will be to load a workbook in your R session with loadWorkbook(); this function will build a "bridge" between your Excel file and your R session.
+In this and the following exercises, you will continue to work with urbanpop.xlsx, containing urban population data throughout time. The Excel file is available in your current working directory.
+
