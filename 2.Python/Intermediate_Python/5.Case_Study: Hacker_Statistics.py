aa0f4074252e393786157ebd4ed521cb9668401d
magatha <77102773+magatha@users.noreply.github.com>
2021-04-11 22:19:16 +0200

    Update 5.Case_Study: Hacker_Statistics.py

--- 2.Python/Intermediate_Python/5.Case_Study: Hacker_Statistics.py
+++ 2.Python/Intermediate_Python/5.Case_Study: Hacker_Statistics.py
@@ -303,3 +303,10 @@ plt.hist(ends)
 plt.show()
 
 -----
+
+#Calculate the odds
+The histogram of the previous exercise was created from a Numpy array ends, that contains 500 integers. Each integer represents the end point of a random walk. To calculate the chance that this end point is greater than or equal to 60, you can count the number of integers in ends that are greater than or equal to 60 and divide that number by 500, the total number of simulations.
+Well then, what's the estimated chance that you'll reach 60 steps high if you play this Empire State Building game? The ends array is everything you need; it's available in your Python session so you can make calculations in the IPython Shell.
+
+78.4%
+-----
